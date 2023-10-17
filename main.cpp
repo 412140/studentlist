9a9885eff3998450bc6e072dbf7b89cb6b1af032
Abbasg1 <spearmints52@gmail.com>
2023-10-17 02:02:50 +0100

    testing mostly

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostrean>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  Struct stdnt
+    {
+      int sid;
+      float gpa;
+      char first[20];
+      char last[20];
+    }
+  char rsp[1];
+  cout <<"Do" << endl;
+  if(tolower.(cin.get(rsp)) =='y')
+    {
+      
+    }
+))
+}
+
